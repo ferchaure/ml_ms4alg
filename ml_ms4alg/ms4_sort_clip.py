bdd1db0f57d68832b6f32cb0aacbb69998dcdcaf
Fernando J. Chaure <ferchaure@users.noreply.github.com>
2020-02-14 21:06:27 -0300

    Create ms4_sort_clip.py

---
+++ ml_ms4alg/ms4_sort_clip.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def sort_clip(clips, num_features = 10, max_num_clips_for_pca=1000)
+
+    # Subsample and extract clips for pca
+    inds=np.random.choice(clisp.shape[0], max_num_clips_for_pca, replace=False)
+
+    # use twice as many features, because of branch method
+    principal_components=compute_principal_components(clips[inds,:],num_features*2) # (MT x 2F)
+
+    # Compute the features for all the clips
+    features=np.zeros((num_features*2,len(times)))
+    features=principal_components.transpose() @ clips # (2F x MT) @ (MT x L0) -> (2F x L0)   
+    labels=branch_cluster(features,branch_depth=2,npca=num_features)
+return labels
